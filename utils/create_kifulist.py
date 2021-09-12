ffe79c4413e21ed704d047887bd0c3bc73efc2ce
Shinji Oda <yanyan0207@gmail.com>
2021-09-12 14:23:13 +0900

    feat: kifulist load処理

--- utils/create_kifulist.py
+++ utils/create_kifulist.py
@@ -5,43 +5,7 @@ import os
 import sys
 import glob
 import logging
-import shogi
-import shogi.CSA
-
-
-def read_csa(file, root_dir=None):
-    try:
-        kifu = shogi.CSA.Parser.parse_file(file)[0]
-        if len(kifu['moves']) == 0:
-            logging.warning(f'{file} no move')
-        elif kifu['sfen'] != 'lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1':
-            logging.warning(f'{file} is not hirate')
-        else:
-            black_rate = -1
-            white_rate = -1
-
-            with open(file) as f:
-                for line in f:
-                    line = line.rstrip()
-                    if line[:len("'black_rate")] == "'black_rate":
-                        black_rate = float(line[line.rfind(':') + 1:])
-                    elif line[:len("'white_rate")] == "'white_rate":
-                        white_rate = float(line[line.rfind(':') + 1:])
-                        break
-                if root_dir is not None:
-                    file = os.path.relpath(file, root_dir)
-            return {
-                'filename': file,
-                'black_player': kifu['names'][0],
-                'white_player': kifu['names'][1],
-                'black_rate': black_rate,
-                'white_rate': white_rate,
-                'move_num': len(kifu['moves']),
-                'win': kifu['win']
-            }
-    except ValueError as e:
-        logging.warning(f"{file} parse_file error:{e}")
-
+from pydlshogi import kifulist
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s\t%(levelname)s\t%(message)s',
@@ -68,9 +32,10 @@ if __name__ == "__main__":
         logging.error("no file")
         sys.exit(1)
 
-    kifu_list = [read_csa(file, root_dir=root_dir) for file in filelist]
+    kifu_list = [kifulist.read_csa(file, root_dir=root_dir)
+                 for file in filelist]
     kifu_list = [kifu_info for kifu_info in kifu_list if kifu_info is not None]
     df = pd.DataFrame.from_dict(kifu_list)
 
     logging.info(f'file num {df.shape[0]}')
-    df.to_csv(ofilename)
+    df.to_csv(ofilename, index=False)

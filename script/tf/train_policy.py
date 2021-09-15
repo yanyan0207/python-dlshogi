71f2339d53ff9005525c88dd755c02160162c0b6
Shinji Oda <yanyan0207@gmail.com>
2021-09-16 08:04:53 +0900

    feat: tf.data.DataSetで書き換え、Permuteの次元誤りを修正

--- script/tf/train_policy.py
+++ script/tf/train_policy.py
@@ -47,7 +47,7 @@ def createModel(blocks):
                    activation='relu', padding='same')(x)
     x = Conv2D(27, kernel_size=(1, 1),
                activation='relu', use_bias=False)(x)
-    x = Permute((2, 3, 1))(x)
+    x = Permute((3, 1, 2))(x)
     x = Flatten()(x)
     x = BiasLayer()(x)
     outputs = x
@@ -90,7 +90,10 @@ if __name__ == "__main__":
     logger = logging.getLogger()
     logger.info('read kifu start')
     # 訓練用とテスト用の棋譜リストを取得
+    time_load_position_list = TimeLog("load_position_list")
+    time_load_position_list.start()
     df_positionlist_train, df_positionlist_test = load_positionlist(args)
+    time_load_position_list.end()
     logger.info(f'train:{df_positionlist_train.describe()}')
     logger.info(f'test:{df_positionlist_test.describe()}')
 
@@ -98,11 +101,11 @@ if __name__ == "__main__":
                == 'F_' and c != 'F_current_movenum']
     single_board_list_train = df_positionlist_train.loc[:, columns].values
     move_train = df_positionlist_train['L_hand'].values
-    #win_train = df_positionlist_train['L_win'].values
+    # win_train = df_positionlist_train['L_win'].values
 
     single_board_list_test = df_positionlist_test.loc[:, columns].values
     move_test = df_positionlist_test['L_hand'].values
-    #win_test = df_positionlist_test['L_win'].values
+    # win_test = df_positionlist_test['L_win'].values
 
     train_num = len(single_board_list_train)
     test_num = len(single_board_list_test)
@@ -137,118 +140,27 @@ if __name__ == "__main__":
     # Instantiate a loss function.
     loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-    if not args.train_loop:
-        time_mini_batch.start()
-        x_train = make_input_features_from_single_board_list(
-            single_board_list_train)
-        t1_train = move_train
-        time_mini_batch.end()
-        logger.info(f'x_train num = {train_num}')
-        logger.info(f'x_train bytessize = {size(x_train.nbytes)}')
+    train_dataset = tf.data.Dataset.from_tensor_slices(
+        (single_board_list_train, move_train))
+    test_dataset = tf.data.Dataset.from_tensor_slices(
+        (single_board_list_test, move_test))
 
-        time_val_mini_batch.start()
-        x_test = make_input_features_from_single_board_list(
-            single_board_list_test)
-        t1_test = move_test
-        time_val_mini_batch.end()
-        logger.info(f'x_test num = {test_num}')
-        logger.info(f'x_test bytessize = {size(x_test.nbytes)}')
+    train_dataset = train_dataset.batch(32).map(lambda x, y:
+                                                (tf.numpy_function(func=make_input_features_from_single_board_list, inp=[x], Tout=tf.int8), y))
+    test_dataset = test_dataset.batch(32).map(lambda x, y:
+                                              (tf.numpy_function(func=make_input_features_from_single_board_list, inp=[x], Tout=tf.int8), y))
 
-        time_train.start()
-        model.compile(optimizer=optimizer, loss=loss_fn,
-                      metrics=['accuracy'])
-        model.fit(x_train, t1_train, epochs=epochs,
-                  batch_size=batchsize, validation_data=(x_test, t1_test), verbose=1)
-        time_train.end()
+    logger.info(f'x_train num = {train_num}')
+    logger.info(f'x_test num = {test_num}')
 
-        logging.info('save the model')
-        model.save(save_model)
+    time_train.start()
+    model.compile(optimizer=optimizer, loss=loss_fn,
+                  metrics=['accuracy'])
+    model.fit(train_dataset, epochs=epochs, batch_size=batchsize,
+              validation_data=test_dataset, verbose=1)
+    time_train.end()
 
-    else:
-        # Prepare the metrics.
-        train_acc_metric = keras.metrics.SparseCategoricalAccuracy()
-        val_acc_metric = keras.metrics.SparseCategoricalAccuracy()
-        for e in range(epochs):
-            logger.info("\nStart of epoch %d" % (e+1,))
-
-            # Todo shuffle
-
-            sum_loss = []
-            for step, i in enumerate(range(0, train_num - batchsize, batchsize)):
-                time_mini_batch.start()
-                x = make_input_features_from_single_board_list(
-                    single_board_list_train[i:i+batchsize])
-                t1 = move_train[i:i+batchsize]
-                #t2 = win_train[i:i+batchsize]
-                time_mini_batch.end()
-
-                time_train.start()
-                # Open a GradientTape to record the operations run
-                # during the forward pass, which enables auto-differentiation.
-                with tf.GradientTape() as tape:
-
-                    # Run the forward pass of the layer.
-                    # The operations that the layer applies
-                    # to its inputs are going to be recorded
-                    # on the GradientTape.
-                    # Logits for this minibatch
-                    logits = model(x, training=True)
-
-                    # Compute the loss value for this minibatch.
-                    loss_value = loss_fn(t1, logits)
-
-                # Use the gradient tape to automatically retrieve
-                # the gradients of the trainable variables with respect to the loss.
-                grads = tape.gradient(loss_value, model.trainable_weights)
-
-                # Run one step of gradient descent by updating
-                # the value of the variables to minimize the loss.
-                optimizer.apply_gradients(zip(grads, model.trainable_weights))
-
-                # Update training metric.
-                sum_loss.append(loss_value)
-                train_acc_metric.update_state(t1, logits)
-                time_train.end()
-
-                # print train loss and test accuracy
-                if (step + 1) % eval_interval == 0:
-                    time_val_mini_batch.start()
-                    randomi = random.randint(0, max(0, test_num - batchsize))
-                    x = make_input_features_from_single_board_list(
-                        single_board_list_test[randomi:randomi+batchsize])
-                    t1 = move_test[randomi:randomi+batchsize]
-                    #t2 = win_test[i:i+batchsize]
-                    y1 = model(x, training=False)
-                    loss_value = loss_fn(t1, y1)
-                    logger.info('epoch = {}, iteration = {}, loss = {}, accuracy = {}'.format(
-                        e + 1, step + 1, np.average(sum_loss),
-                        np.sum(np.sum(np.argmax(y1, axis=1) == t1)) / len(t1)))
-                    sum_loss = []
-                    time_val_mini_batch.end()
-
-            # Display metrics at the end of each epoch.
-            train_acc = train_acc_metric.result()
-            logger.info("Training acc over epoch: %.4f" % (float(train_acc),))
-
-            # Reset training metrics at the end of each epoch
-            train_acc_metric.reset_states()
-
-            time_val_epoch.start()
-            # Run a validation loop at the end of each epoch.
-            for step in range(0, test_num - batchsize, batchsize):
-
-                x_batch_val = make_input_features_from_single_board_list(
-                    single_board_list_test[step:step+batchsize])
-                y_batch_val = move_test[step:step+batchsize]
-                val_logits = model(x_batch_val, training=False)
-                # Update val metrics
-                val_acc_metric.update_state(y_batch_val, val_logits)
-            val_acc = val_acc_metric.result()
-            val_acc_metric.reset_states()
-            logger.info("Validation acc: %.4f" % (float(val_acc),))
-            time_val_epoch.end()
-
-        logging.info('save the model')
-        model.save(save_model)
+    logging.info('save the model')
+    model.save(save_model)
 
     logger.info(f'\n{TimeLog.debug()}')

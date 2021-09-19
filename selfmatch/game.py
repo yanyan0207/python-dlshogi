be2f8ed7f1815573e2b5231fd2ed6960ae1a8b7e
Shinji Oda <yanyan0207@gmail.com>
2021-09-19 19:47:06 +0900

    feat: PolicyPlayerの初期実装

--- selfmatch/game.py
+++ selfmatch/game.py
@@ -1,99 +1,162 @@
+from os import name
 import shogi
 import copy
 import random
 from enum import Enum
 from shogi.Consts import BLACK, WHITE
+import time
 
 
 class Player():
+    def __init__(self, name='Player'):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
     def setConfig(self, key, value):
+        pass
+
+    def prepare(self):
         pass
 
     def startMatch(self):
         pass
 
-    def bestMove(self, board: shogi.Board):
-        # 合法手の数を算出
-        legal_moves = [move for move in board.legal_moves]
-
+    def bestMove(self, board: shogi.Board, legal_moves: 'set[shogi.Move]'):
         # 手がない場合は負け
-
         if len(legal_moves) == 0:
             return None
-
         # ランダムに手を返す
         else:
-            return legal_moves[random.randint(0, len(legal_moves)) - 1]
+            return list(legal_moves)[random.randint(0, len(legal_moves)) - 1]
+
+
+class Reason(Enum):
+    CHECK_MATE = 'CHECK_MATE'
+    TORYO = 'TORYO'
+    ILLEGAL_MOVE = 'ILLEGAL_MOVE'
+    SENNICHITE = 'SENNICHITE'
+    MAX_MOVE = 'MAX_MOVE'
+    # TODO TIMEOUT = 'TIMEOUT'
+    # TPDP TRU = 'TRY'
+
+
+class Result(Enum):
+    BLACK_WIN = 'BLACK_WIN'
+    WHITE_WIN = 'WHITE_WIN'
+    DRAW = 'DRAW'
+
+
+class GameResult():
+
+    def __init__(self, black: Player, white: Player, startTime: float = None):
+        self.black_player_name = black.name
+        self.white_player_name = white.name
+        self.reason = None
+        self.result = None
+        self.move_num = None
+        self.startTime = time.time() if startTime is None else startTime
+        self.endTime = None
+
+    def setResult(self, result: Result, reason: Reason, move_num: int, endTime: float = None):
+        self.reason = reason
+        self.result = result
+        self.move_num = move_num
+        self.endTime = time.time() if endTime is None else endTime
 
 
 class Game():
-    class EndReason(Enum):
-        TORYO = "TORYO"
-        ILLEGAL_MOVE = "ILLEGAL_MOVE"
-        SENNICHITE = "SENNICHITE"
-        TSUMI = "TSUMI"
-        # TODO TIMEOUT = "TIMEOUT"
-        # TPDP TRU = "TRY"
-
-    class EndResult(Enum):
-        BLACK_WIN = "BLACK_WIN"
-        WHITE_WIN = "WHITE_WIN"
-        DRAW = "DRAW"
-
-    @staticmethod
+    @ staticmethod
     def opposit(turn: int):
         return shogi.WHITE if turn == shogi.BLACK else shogi.BLACK
 
-    @classmethod
+    @ classmethod
     def endResult(cls, turn: int):
-        return turn == cls.EndResult.BLACK_WIN if turn == shogi.BLACK else cls.EndResult.WHITE_WIN
+        return Result.BLACK_WIN if turn == shogi.BLACK else Result.WHITE_WIN
 
-    def __init__(self):
-        self.players = [Player(), Player()]
+    def __init__(self, black=Player(), white=Player()):
+        self.players = [black, white]
         self.board = shogi.Board()
+        self.display_kifu = True
+        self.game_result = None
+        self.max_move = 256
+
+    def setDisplayKif(self, display):
+        self.display_kifu = display
 
     def setPlayer(self, color: int, player: Player):
         self.players[color] = player
 
-    def setConfig(self, color: int, key, value):
+    def setPlayerConfig(self, color: int, key, value):
         self.players[color].setConfig(key, value)
 
-    def startMatch(self):
+    def prepare(self):
+        for player in self.players:
+            player.prepare()
+
+    def playMatch(self) -> GameResult:
         self.board.reset()
+        self.game_result = GameResult(
+            self.players[0], self.players[1])
         for player in self.players:
             player.startMatch()
 
+        while not self.processOneMove():
+            pass
+
+        return self.game_result
+
     def processOneMove(self):
-        print(f'{self.board.move_number}手目')
+        if self.display_kifu:
+            print(f'{self.board.move_number}手目')
         end = True
 
         turn = self.board.turn
         opposit = self.opposit(turn)
         player = self.players[self.board.turn]
-        move = player.bestMove(copy.deepcopy(self.board))
+        legal_moves = {move for move in self.board.legal_moves}
+
+        move = player.bestMove(copy.deepcopy(self.board), legal_moves)
 
         # 投了の場合
         if move is None:
             # 投了
-            self.win = self.endResult(opposit)
-            self.end_reason = Game.EndReason.TORYO
-            print(f'{"先手" if turn == shogi.BLACK else "後手"}投了')
+            self.game_result.setResult(self.endResult(
+                opposit), Reason.TORYO, self.board.move_number)
+            if self.display_kifu:
+                print(f'{"先手" if turn == shogi.BLACK else "後手"}投了')
 
         # NullMoveチェック/合法手チェック
-        elif not (move and move in self.board.pseudo_legal_moves):
-            self.win = self.endResult(opposit)
-            self.end_reason = Game.EndReason.ILLEGAL_MOVE
-            print(f'{"先手" if turn == shogi.BLACK else "後手"}反則')
+        elif not (move and move in legal_moves):
+            self.game_result.setResult(self.endResult(
+                opposit), Reason.ILLEGAL_MOVE, self.board.move_number)
+            if self.display_kifu:
+                print(f'{"先手" if turn == shogi.BLACK else "後手"}反則')
         else:
-            print(moveToStr(move, self.board))
+            if self.display_kifu:
+                print(moveToStr(move, self.board))
+
+            # 手を進める
             self.board.push(move)
-            print(self.board.kif_str())
+
+            if self.display_kifu:
+                print(self.board.kif_str())
+
             # 千日手チェック
             # TODO 王手千日手チェック
             if self.board.is_fourfold_repetition():
-                self.win = self.EndResult.DRAW
-                self.end_reason = Game.EndReason.SENNICHITE
-                print('千日手')
+                self.game_result.setResult(
+                    Result.DRAW, Reason.SENNICHITE, self.board.move_number)
+                if self.display_kifu:
+                    print('千日手')
+            # 最大手数チェック
+            elif self.board.move_number == self.max_move:
+                self.game_result.setResult(
+                    Result.DRAW, Reason.MAX_MOVE, self.board.move_number)
+                if self.display_kifu:
+                    print('最大手数')
+
             else:
                 # 通常ルート
                 end = False
@@ -114,8 +177,11 @@ def moveToStr(move: shogi.Move, board: shogi.Board):
         return f'{turn}{to_sq}{koma}{promotion} ({from_sq})'
 
 
-if __name__ == "__main__":
-    game = Game()
-    while not game.processOneMove():
-        pass
-    print(game.win, game.end_reason)
+if __name__ == '__main__':
+    match = Game()
+    result = match.playMatch()
+
+    print(f'black:{result.black_player_name}')
+    print(f'white:{result.white_player_name}')
+    print(f'{result.move_num}手')
+    print(f'{result.result.value} {result.reason.value}')
---
+++ selfmatch/policy_player.py
@@ -0,0 +1,118 @@
+import shogi
+import shogi.CSA
+import game
+import os
+from pydlshogi.features import *
+import scipy as sp
+import numpy as np
+import glob
+import math
+
+if True:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+from tensorflow import keras
+
+
+class PolicyPlayer(game.Player):
+    def __init__(self, name='PolicyPlayer'):
+        self.name = name
+        self.modelPath = None
+        self.weightsPath = None
+        self.model: keras.models.model = None
+
+    def setConfig(self, key, value):
+        if key == 'ModelPath':
+            self.modelPath = value
+        elif key == 'WeightsPath':
+            self.weightsPath = value
+
+    def prepare(self):
+        self.model = keras.models.load_model(self.modelPath)
+        if self.weightsPath:
+            self.model.load_weights(self.weightsPath)
+
+    def startMatch(self):
+        pass
+
+    def bestMove(self, board: shogi.Board, legal_moves: 'set[shogi.Move]'):
+        # 手がない場合は負け
+        if len(legal_moves) == 0:
+            return None
+
+        features = make_input_features_from_board(board)
+        logits = self.model.predict(features)[0]
+        probabilities = sp.special.softmax(logits)
+        moves = [(move, make_output_label(move, board.turn))
+                 for move in legal_moves]
+        probs = [probabilities[move_label] for move, move_label in moves]
+        return moves[np.argmax(probs)][0]
+
+
+if __name__ == "__main__":
+    class Standing:
+        def __init__(self):
+            self.win = 0
+            self.lose = 0
+            self.draw = 0
+            self.win_rate = 0
+
+        def addWin(self):
+            self.win += 1
+            self.win_rate = self.win / (self.win + self.lose)
+
+        def addLose(self):
+            self.lose += 1
+            self.win_rate = self.win / (self.win + self.lose)
+
+        def addDraw(self):
+            self.draw += 1
+
+    class StandingList():
+        def __init__(self, name_list: 'list[str]' = None):
+            self.standing_list: dict[str, Standing] = {}
+            if name_list is not None:
+                self.addPlayerNameList(name_list)
+
+        def addPlayerName(self, name: str):
+            self.standing_list[str(name)] = Standing()
+
+        def addPlayerNameList(self, name_list: 'list[str]'):
+            [self.addPlayerName(name) for name in name_list]
+
+        def addResult(self, result: game.GameResult):
+            if result.result == game.Result.BLACK_WIN:
+                self.standing_list[result.black_player_name].addWin()
+                self.standing_list[result.white_player_name].addLose()
+            elif result.result == game.Result.WHITE_WIN:
+                self.standing_list[result.black_player_name].addLose()
+                self.standing_list[result.white_player_name].addWin()
+            else:
+                assert(result.result == game.Result.DRAW)
+                self.standing_list[result.black_player_name].addDraw()
+                self.standing_list[result.white_player_name].addDraw()
+
+    ckpt_index_list = glob.glob(
+        '/Users/yanyano0207/Downloads/model_2017_policy/weights*.ckpt.index')
+    player_list = [game.Player()] + [PolicyPlayer(os.path.basename(ckpt))
+                                     for ckpt in ckpt_index_list][:1]
+
+    for player, ckpt in zip(player_list, ckpt_index_list):
+        player.setConfig(
+            'ModelPath', '/Users/yanyano0207/Downloads/model_2017_policy/model/')
+        player.setConfig('WeightsPath', ckpt[:-6])
+        player.prepare()
+
+    player_match_list = [(player, player2)
+                         for player in player_list for player2 in player_list if player != player2]
+
+    standing_list = StandingList(player_list)
+    for player, player2 in player_match_list:
+        match = game.Game(player, player2)
+        match.setDisplayKif(False)
+        result = match.playMatch()
+        print(result.black_player_name, result.white_player_name,
+              result.move_num, result.result, result.reason)
+        standing_list.addResult(result)
+
+    for name, standing in standing_list.standing_list.items():
+        print(f'{name} {standing.win} - {standing.lose} - {standing.draw} {round(standing.win_rate * 100)}%')

47fcddba9bc3072b223167ece70d4f2b9a75183a
Shinji Oda <yanyan0207@gmail.com>
2021-10-04 18:07:30 +0900

    feat: モデル内の勝率のグラフ出力

--- pydlshogi_v2/trainer/plot_history.py
+++ pydlshogi_v2/trainer/plot_history.py
@@ -17,12 +17,19 @@ if __name__ == "__main__":
                 model_root), color=color_list[i], linestyle='dashed')
             plt.plot(df.val_policy_output_loss, label=os.path.basename(
                 model_root), color=color_list[i])
-
+    plt.legend()
     plt.figure()
     for i, model_root in enumerate(args.model_root):
         if os.path.exists(f'{model_root}/history.csv'):
             df = pd.read_csv(f'{model_root}/history.csv')
             plt.plot(df.val_policy_output_accuracy, label=os.path.basename(
+                model_root), color=color_list[i])
+    plt.legend()
+    plt.figure()
+    for i, model_root in enumerate(args.model_root):
+        if os.path.exists(f'{model_root}/model_match.csv'):
+            df = pd.read_csv(f'{model_root}/model_match.csv')
+            plt.plot(df.win_rate, label=os.path.basename(
                 model_root), color=color_list[i])
     plt.legend()
     plt.show()

be6784efc9826f4ccf5c983a4e7b0fd6d1830181
Shinji Oda <yanyan0207@gmail.com>
2021-10-05 12:57:46 +0900

    chore: python-dlshogiをインポート対象外にする

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 ﻿import setuptools
 
 setuptools.setup(
-    name='python-dlshogi',
-    version='0.0.1',
-    author='',
-    packages=['pydlshogi'],
-    scripts=[],
-)
-
-
-setuptools.setup(
     name='python-dlshogi_v2',
     version='0.1.0',
     author='',

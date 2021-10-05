14f17b3da2a8c3a6eb12802f9fb673e512c7b2be
Shinji Oda <yanyan0207@gmail.com>
2021-10-05 13:14:50 +0900

    chore: 訓練スクリプト、データファイルを引数指定に変更

--- script/train_v2.py
+++ script/train_v2.py
@@ -4,25 +4,23 @@ import os
 
 if __name__ == "__main__":
     parser = ArgumentParser()
+    parser.add_argument('train_data_list')
+    parser.add_argument('test_data_list')
     parser.add_argument('--batch_size', nargs='*', type=int)
     parser.add_argument('--learning_rate', nargs='*', type=float)
     parser.add_argument('--epoch', type=int)
     args = parser.parse_args()
 
     train_py = os.path.join(os.path.dirname(__file__),
-                            'pydlshogi_v2/trainer/trainer_v2_sfen.py')
-    train_data_list = os.path.join(
-        os.environ['HOME'], 'data/floodgate/data_list_2017_minrate_2500_minmove_50.csv')
-    test_data_list = os.path.join(
-        os.environ['HOME'], 'data/floodgate/data_list_2020_minrate_4000_minmove_50.csv')
+                            '../pydlshogi_v2/trainer/trainer_v2_sfen.py')
 
     for batch_size in args.batch_size:
         for lr in args.learning_rate:
             try:
                 model_dir = os.path.join(os.path.dirname(
-                    __file__), f'training_results/trainer_v2-batchsize_{batch_size}-lr_{lr}')
+                    __file__), f'../training_results/trainer_v2-batchsize_{batch_size}-lr_{lr}')
                 os.mkdir(model_dir)
-                cmd = f'python {train_py} {train_data_list} {test_data_list}'
+                cmd = f'python3 {train_py} {args.train_data_list} {args.test_data_list}'
                 cmd += f' --model {model_dir}'
                 cmd += f' --batch_size {batch_size}'
                 cmd += f' --learning_rate {lr}'
